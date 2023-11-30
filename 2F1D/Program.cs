2248e3afaf81ec3556476401e375bf057f035c6d
MkGodh <fenryrvla@yandex.ru>
2023-11-30 23:32:13 +0300

    Add code

---
+++ 2F1D/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿int count = 0;
+int distance = 10000;
+int FS1 = 1;
+int FS2 = 2;
+int Dogspeed = 5;
+int friend = 2;
+int time = 0;
+
+while(distance > 10)
+{
+   if(friend == 1)
+   {
+   time = distance / (FS1 + Dogspeed);
+   friend = 2;
+   }
+   else
+   {
+   time = distance / (FS2 + Dogspeed);
+   friend = 1;
+   }
+   distance = distance - (FS1 +FS2) * time;
+   count += 1;
+}
+
+Console.WriteLine("Собака пробежит " + count + " раз.");
